4f865e8383451099c68b66746b3318fc86f74b4c
Yuri Furquim <yuripatrick347@gmail.com>
2020-09-01 15:25:52 -0300

    Projeto Criado

---
+++ src/test/java/com/mcspringboot/McSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
